eb98b4f97762feb37f8a65fa7f4e2fdb2251b8d6
fxleblanc <felix.leblanc1305@gmail.com>
2016-06-18 11:04:24 -0400

    models task

--- gulpfile.js
+++ gulpfile.js
@@ -78,11 +78,10 @@ gulp.task('images', () => {
     .pipe(gulp.dest('dist/images'));
 });
 
-//Task not working properly
-// gulp.task('models', () => {
-//   return gulp.src('app/models/**/*')
-//     .pipe(gulp.dest('dist/models'))
-// });
+gulp.task('models', () => {
+  return gulp.src('app/models/**/*')
+    .pipe(gulp.dest('dist/models'))
+});
 
 gulp.task('fonts', () => {
   return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
@@ -171,7 +170,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', [ 'lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', [ 'lint', 'html', 'images', 'fonts', 'extras', 'models'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
